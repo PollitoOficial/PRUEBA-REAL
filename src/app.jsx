5eef2fdf576490dd654258a4e70536733c537bfe
PollitoOficial <emiliojesuscs16@gmail.com>
2025-07-03 18:53:21 -0500

    Introduccion de plantilla

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Hola desde React + Vite</h1>
+    </div>
+  )
+}
