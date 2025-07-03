448d47260d378c9cdd3c2d7bc20716508e38382f
PollitoOficial <emiliojesuscs16@gmail.com>
2025-07-03 13:33:47 -0500

    Primer commit

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Asegúrate de que Vite construya en la carpeta correcta
+  },
+})
